49765e0b884ea65c670a55686a24b19d8243c767
2hoyeong <leehodud302@naver.com>
2021-03-11 00:10:37 +0900

    feat : 폰트 설정
    폰트를 나눔고딕 600, Noto Sans 400으로 설정하고 타이틀을 YouTube로 변경

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Noto Sans KR', 'Nanum Gothic', sans-serif;
+    }
     body {
         width: 100%;
         margin: 0 auto;
