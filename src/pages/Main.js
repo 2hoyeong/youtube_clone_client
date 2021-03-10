e649feed0ac562080c6a83a7ae03cb78eceb6865
2hoyeong <leehodud302@naver.com>
2021-03-11 00:19:01 +0900

    feat : Header 템플릿 적용
    메인 페이지에 Header 템플릿을 적용

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../templates/Header';
 
 const Main = () => {
-  return <div>메인페이지입니다.</div>;
+  return (
+    <>
+      <Header />
+      <div>메인페이지입니다.</div>
+    </>
+  );
 };
 
 export default Main;
