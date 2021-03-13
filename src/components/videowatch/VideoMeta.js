ae1351307d6048f3247345172253f36f82c85460
2hoyeong <leehodud302@naver.com>
2021-03-14 03:22:03 +0900

    feat : VideoMeta 컴포넌트 구현
    비디오 시청 페이지에서 제목, 조회수, 날짜, 업로더가 표시되는 컴포넌트 구현

---
+++ src/components/videowatch/VideoMeta.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const VideoMeta = ({ title, views, date, profileImg, uploader }) => {
+  return (
+    <Container>
+      <div>
+        <TitleDiv>{title ? title : '-'}</TitleDiv>
+        <MetaDiv>
+          조회수 {views}회 • {date}
+        </MetaDiv>
+      </div>
+      <FlexDiv>
+        <CircleDiv>
+          <ProfileImg src={profileImg} />
+        </CircleDiv>
+        <UploaderDiv>{uploader}</UploaderDiv>
+      </FlexDiv>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding-top: 20px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TitleDiv = styled.div`
+  color: #030303;
+  font-size: 18px;
+  font-weight: 400;
+`;
+
+const MetaDiv = styled.div`
+  color: #606060;
+  font-size: 14px;
+`;
+
+const CircleDiv = styled.div`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin-right: 12px;
+`;
+
+const ProfileImg = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+
+const UploaderDiv = styled.div`
+  color: #606060;
+`;
+
+const FlexDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export default VideoMeta;
