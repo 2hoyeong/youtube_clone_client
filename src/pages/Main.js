ff99c45b9b6c896b1c578d0e154e801008f8e9ba
2hoyeong <leehodud302@naver.com>
2021-03-14 19:19:56 +0900

    feat : 메인 페이지 비디오 리스트 구현
    메인 페이지에서 서버로 비디오 리스트를 받아와 출력하도록 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../templates/Header';
 import VideoList from '../templates/VideoList';
+import useAsync from '../lib/hooks/useAsync';
+import instance from '../lib/apis/instance';
+
+const getRandomVideoList = async () => {
+  const response = await instance.get('/api/v1/video/list');
+  return response.data;
+};
 
 const Main = () => {
+  const [videoList, reloadVideoList] = useAsync(getRandomVideoList, [], true);
+
+  useEffect(() => {
+    reloadVideoList();
+  }, []);
+
   return (
     <>
       <Header />
-      <VideoList videoList={TestVideo} />
+      {videoList.data ? (
+        <VideoList videoList={videoList.data} />
+      ) : (
+        <div style={{ 'text-align': 'center' }}>업로드된 비디오가 없습니다.</div>
+      )}
     </>
   );
 };
