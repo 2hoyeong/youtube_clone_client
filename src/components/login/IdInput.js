f95cc61be9a7d78cf32214df69ac6f5edd4b2f62
2hoyeong <leehodud302@naver.com>
2021-03-22 02:47:01 +0900

    feat : 아이디 인풋 컴포넌트 디자인 구현
    아이디 인풋 컴포넌트 디자인 구현

---
+++ src/components/login/IdInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const IdInput = () => {
+  return (
+    <>
+      <StyledInput placeholder={'이메일'} />
+    </>
+  );
+};
+
+const StyledInput = styled.input`
+  color: #202124;
+  font-size: 17px;
+  height: 28px;
+  margin: 1px 1px 0 1px;
+  padding: 13px 15px;
+  border-radius: 4px;
+  border: 1px solid #dadce0;
+  background-color: transparent;
+  outline: none;
+  width: 100%;
+`;
+
+export default IdInput;
