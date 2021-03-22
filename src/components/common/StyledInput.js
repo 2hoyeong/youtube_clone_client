70fd8c228ec4fb784b5194e5720dedaf30a5cac2
2hoyeong <leehodud302@naver.com>
2021-03-22 18:01:39 +0900

    refactor : 같은 스타일 input 재사용 하기 위해 변경
    재사용을 위해 컴포넌트 분리

---
+++ src/components/common/StyledInput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledInput = (child) => {
+  return <Input {...child} />;
+};
+
+const Input = styled.input`
+  color: #202124;
+  font-size: 17px;
+  height: 28px;
+  margin: 1px 1px 0 1px;
+  padding: 13px 15px;
+  border-radius: 4px;
+  border: 1px solid #dadce0;
+  background-color: transparent;
+  outline: none;
+  width: 100%;
+`;
+
+export default StyledInput;
