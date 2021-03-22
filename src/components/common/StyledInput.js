1889e446dbac1ed5676fb554a371f95680446dd0
2hoyeong <leehodud302@naver.com>
2021-03-23 03:21:26 +0900

    beauty : padding 제거
    StyledInput 컴포넌트 가로 padding 제거

--- src/components/common/StyledInput.js
+++ src/components/common/StyledInput.js
@@ -10,7 +10,7 @@ const Input = styled.input`
   font-size: 17px;
   height: 28px;
   margin: 1px 1px 0 1px;
-  padding: 13px 15px;
+  padding: 13px 0;
   border-radius: 4px;
   border: 1px solid #dadce0;
   background-color: transparent;
