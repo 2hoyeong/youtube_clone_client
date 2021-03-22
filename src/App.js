d3e418e420cc1b4e6e5546261fef1fd5297622ef
2hoyeong <leehodud302@naver.com>
2021-03-23 03:48:24 +0900

    feat : 로그인 페이지 라우팅
    로그인 페이지 라우팅

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import { GlobalStyle } from './GlobalStyle';
 import VideoWatch from './pages/VideoWatch';
+import Login from './pages/Login';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/watch/:videoId" component={VideoWatch} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </>
   );
