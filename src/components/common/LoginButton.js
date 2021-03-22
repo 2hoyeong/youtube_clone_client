94291294e7fb26017525f60b1f086e14942bcb74
2hoyeong <leehodud302@naver.com>
2021-03-22 18:29:42 +0900

    feat : 로그인 버튼 로그인 페이지 연동
    로그인 버튼을 누르면 로그인 페이지로 이동되도록 구현

--- src/components/common/LoginButton.js
+++ src/components/common/LoginButton.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { ReactComponent as User } from '../../assets/user.svg';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const LoginButton = () => {
   return (
-    <FlexDiv>
-      <TestDiv>
-        <User width="24px" height="24px" fill="#3ea6ff" />
-        <LoginLabel>로그인</LoginLabel>
-      </TestDiv>
-    </FlexDiv>
+    <Link to={`/login`}>
+      <FlexDiv>
+        <TestDiv>
+          <User width="24px" height="24px" fill="#3ea6ff" />
+          <LoginLabel>로그인</LoginLabel>
+        </TestDiv>
+      </FlexDiv>
+    </Link>
   );
 };
 
