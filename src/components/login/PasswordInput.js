b02e86c183851a858896c2313b55eccdb7bc1270
2hoyeong <leehodud302@naver.com>
2021-03-22 18:01:57 +0900

    feat : 비밀번호 인풋 컴포넌트 구현
    비밀번호 인풋 컴포넌트 구현

---
+++ src/components/login/PasswordInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StyledInput from '../common/StyledInput';
+
+const PasswordInput = () => {
+  return (
+    <>
+      <StyledInput placeholder={'비밀번호'} type={'password'} />
+    </>
+  );
+};
+
+export default PasswordInput;
