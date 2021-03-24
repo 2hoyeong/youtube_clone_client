a40ec83fe34478d414c58c5388d812b9ff1b9458
2hoyeong <leehodud302@naver.com>
2021-03-24 21:38:05 +0900

    feat : NotFoundPage 구현
    NotFoundPage 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from './pages/Main';
 import { GlobalStyle } from './GlobalStyle';
 import VideoWatch from './pages/VideoWatch';
 import Login from './pages/Login';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path="/" component={Main} />
         <Route exact path="/watch/:videoId" component={VideoWatch} />
         <Route exact path="/login" component={Login} />
+        <Route component={NotFoundPage} />
       </Switch>
     </>
   );
