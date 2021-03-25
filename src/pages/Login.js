7a8d03fee19b4bb8dd0fa3625b6f2b6aa13b5790
2hoyeong <leehodud302@naver.com>
2021-03-26 02:24:55 +0900

    feat : Login 기능 구현
    서버와 연동한 Login 기능 구현

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import IdPasswordInput from '../templates/IdPasswordInput';
 import LoginRegistButton from '../templates/LoginRegistButton';
+import useAsync from '../lib/hooks/useAsync';
+import instance from '../lib/apis/instance';
+
+const requestLogin = (user) => async () => {
+  const response = await instance.post(`/api/v1/user/signin`, user);
+  return response.data;
+};
 
 const Login = () => {
   const [user, setUser] = useState({
     id: '',
     password: '',
   });
+
+  const [login, doLogin] = useAsync(requestLogin(user), [], true);
 
   const onChangeIdInput = (event) => {
     setUser({ ...user, id: event.target.value });
@@ -16,6 +25,13 @@ const Login = () => {
   const onChangePasswordInput = (event) => {
     setUser({ ...user, password: event.target.value });
   };
+
+  useEffect(() => {
+    if (login.data?.token) {
+      localStorage.setItem('token', login.data.token);
+      window.location = '/';
+    }
+  }, [login.data]);
 
   return (
     <MaxDiv>
@@ -27,7 +43,7 @@ const Login = () => {
           <BlueSpan>아이디를 잊으셨나요?</BlueSpan>
           <br />내 컴퓨터가 아닌가요? 게스트 모드를 사용하여 비공개로 로그인하세요. <BlueSpan>자세히 알아보기</BlueSpan>
         </div>
-        <LoginRegistButton />
+        <LoginRegistButton onClickLoginButton={doLogin} />
       </FlexDiv>
     </MaxDiv>
   );
--- src/templates/LoginRegistButton.js
+++ src/templates/LoginRegistButton.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 import BlueButton from '../components/common/BlueButton';
 import { Link } from 'react-router-dom';
 
-const LoginRegistButton = () => {
+const LoginRegistButton = ({ onClickLoginButton }) => {
   return (
     <Div>
       <Link to={'/regist'} style={{ color: '#1a73e8' }}>
         계정 만들기
       </Link>
-      <BlueButton text={'로그인'} />
+      <BlueButton text={'로그인'} onClick={onClickLoginButton} />
     </Div>
   );
 };
