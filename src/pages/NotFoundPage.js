51202bb5d40644a6aadaed5859ea19050b917190
2hoyeong <leehodud302@naver.com>
2021-04-16 23:16:18 +0900

    feat : 미 구현 페이지 접속 시 자동 이동 구현
    미 구현된 라우팅에 접근 시 NotFoundPage가 렌더링 되는데, 렌더링 되고 1초 뒤에 메인화면으로 이동하도록 구현

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const NotFoundPage = () => {
-  return <div>Page not found</div>;
+  useEffect(() => {
+    (async () => {
+      setTimeout(() => {
+        window.location = '/';
+      }, 1000);
+    })();
+  }, []);
+  return <div>페이지를 찾을 수 없습니다. 1초 뒤 메인화면으로 이동합니다.</div>;
 };
 
 export default NotFoundPage;
