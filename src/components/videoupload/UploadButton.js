e91d144bbfed286bf3f420251adde5e3222fbb73
2hoyeong <leehodud302@naver.com>
2021-04-17 22:46:20 +0900

    feat : UploadButton 컴포넌트 구현
    UploadButton 컴포넌트를 구현

---
+++ src/components/videoupload/UploadButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Upload } from '../../assets/upload.svg';
+
+const UploadButton = () => {
+  return (
+    <div>
+      <Upload width="24px" height="24px" fill="#A0A0A0" />
+    </div>
+  );
+};
+
+export default UploadButton;
