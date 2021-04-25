c56e8ef6d421de147d6c36a47e3a77f774a9e917
2hoyeong <leehodud302@naver.com>
2021-04-25 23:25:36 +0900

    feat : VideoTitleInput 컴포넌트 생성
    VideoTitleInput 컴포넌트를 생성

---
+++ src/components/videodescript/VideoTitleInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoTitleInput = ({ onChange }) => {
+  return (
+    <>
+      <input onChange={onChange} />
+    </>
+  );
+};
+
+export default VideoTitleInput;
